c974fc52e80531e59c94a85d6fabc02835153a51
Lxr_Dev <alexis_danielGT@hotmail.com>
2020-03-31 11:16:00 -0600

    Añadidos los créditos

--- UNO/WebContent/Javascript/popup.js
+++ UNO/WebContent/Javascript/popup.js
@@ -11,11 +11,11 @@ let cerrar = document.getElementById('close');
 var message1='<h4 id="codeS">CÓDIGO ACCESO</h4><p></p><br><form><button type="button" onclick="generateCode();" class="btn-submit">Generar Código</button>&nbsp;&nbsp;&nbsp;<button type="button" onclick="Start();" class="btn-submit">Iniciar Partida</button></form>';
 var message2='<div id="exception"></div><h4>INGRESE CÓDIGO ACCESO</h4><form onsubmit="return join();"><input id="code" type="text" placeholder="Ingrese código de acceso"><br><button type="submit" class="btn-submit">Unirse</button></form>';
 var message3='<div id="scoreTable" class="container-table"><div class="table-title">Tabla de Ganadores</div><br><br><br><br><br><div id="tablita"></div>';
-var message4='<div id="credits" class="container-table"><div class="table-title">Integrantes</div>'
-+'<div class="table-header">Nombre </div><div class="table-header">#Cta.</div>'
-+'<div class="table-item">name1</div><div class="table-item">20201515963</div><div class="table-item">name2</div>'
-+'<div class="table-item">20201515963</div><div class="table-item">name3</div><div class="table-item">20201515963</div>'
-+'<div class="table-item">name4</div><div class="table-item">20201515963</div><div class="table-footer">POO &copy;</div></div>';
+var message4='<div id="credits" class="container-table"><div class="table-title">- Créditos -</div>'
++'<div class="table-header">Nombre </div><div class="table-header">No. Cuenta</div>'
++'<div class="table-item">Alexandra Lau</div><div class="table-item">20131005945</div><div class="table-item">Alexis Ochoa</div>'
++'<div class="table-item">20161002139</div><div class="table-item">Andrés Zuniga</div><div class="table-item">20161003850</div>'
++'<div class="table-item">Marco Tulio Ruíz</div><div class="table-item">20171006559</div><br><div class="table-footer">POO - Sección 1000 - I PAC 2020 - Ing. José Inestroza</div></div>';
 
 abrir1.addEventListener('click',function(){
 	document.getElementById("message").innerHTML = message1;
